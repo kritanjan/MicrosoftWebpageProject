47b69db87717a17ebdd779d016273b38c33bd528
Kritanjan Bhattacharjee <kritanjan101@gmail.com>
2022-07-25 14:33:15 +0530

    initial commit

---
+++ engine.js
@@ -0,0 +1,7 @@
+// This is an onclick fuction for the initial close button
+function closeButton(){
+    // console.log("close is executed")
+    warning = document.getElementById("warning");
+    warning.style.display = "none";
+}
+
